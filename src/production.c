03fe27e868a816d9b45fa7595fa89bdd17fd993f
Noah-C-S <noahcscs@gmail.com>
2019-02-03 18:18:06 -0500

    first commit

---
+++ src/production.c
@@ -0,0 +1,150 @@
+/*
+ * production.c
+ *
+ *  Created on: Nov 3, 2018
+ *      Author: student
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "production.h"
+#include <math.h>
+#define FILLEDCHAR 'x'
+#define UNFILLEDCHAR 'o'
+
+/** Runs the game of life program
+ * @param argc the amount of arguments in argv
+ * @param argv the arguments passed in the command line
+ * @return true if the program ran properly, false otherwise
+ */
+bool production(int argc, char* argv[])
+{
+	bool results = false;
+	bool done = false;
+	int nRows=-1;
+	int nCols = -1;
+	int gens = 0; //number of generations to play
+	char* inputfile;
+	bool print = false;
+	bool pause = false;
+	FILE *file;
+	//etc.
+	//get the NR NC gens input [print] [pause], Usage as needed.
+
+	// Must first check if there are at least 4 arguments on the command line
+	if(argc < 5){
+		printf("Not enough arguments. Usage: ./HW2 NR(int) NC(int) gens(int) inputfile(str) [print(y/n)] [pause(y/n)]");
+		done = true;
+		return results;
+	}
+	if(argc > 7){
+		printf("Too many arguments. Usage: ./HW2 NR(int) NC(int) gens(int) inputfile(str) [print(y/n)] [pause(y/n)]");
+		done = true;
+		return results;
+	}
+	char* ptr = 0;
+	nRows = (int) strtol(argv[1], &ptr, 10); //rows and columns
+	nCols = (int) strtol(argv[2], &ptr, 10);
+	gens = (int) strtol(argv[3], &ptr, 10); //gens
+	inputfile = argv[4]; //file
+	if(argc > 5){
+		print = argv[5][0] == 'y' || argv[5][0] == 'Y'; //technically, anything starting with y would work (such as "yellow"), but this is ok since
+		if(argc > 6){			   //behavior is undefind for anything except 'y' and 'n'.
+			pause = argv[6][0] == 'y' || argv[6][0] == 'Y'; //I'm too nice for unnecessary undefined behavior, so 'Y' is acceptable too.
+		}
+	}
+	if(nRows < 1){
+		printf("Usage: Rows should be greater than 0, received %d.\n",nRows);
+		done = true;
+	}
+	if(nCols < 1){
+		printf("Usage: Cols should be greater than 0, received %d.\n",nCols);
+		done = true;
+	}
+	if(gens < 1){
+		printf("Usage: gens should be greater than 0, recieved %d.\n",gens);
+	}
+
+	file = fopen(inputfile, "r");
+	if(file == NULL){
+		printf("The file \"%s\" does not exist!", inputfile);
+		done = true;
+	}
+	if(done) return results;
+
+	return results;
+
+}
+void PlayOne (unsigned int nr, unsigned int nc, char Old[][nc], char New[][nc])
+{
+	int nRows = nr;
+	int nCols = nc;
+	for(int row = 0;row < nRows;row++)
+	{
+		for(int col=0; col < nCols; col++)
+		{
+			*( ((char*) New) + (row * nCols) + col) = *( ((char*) Old) + (row * nCols) + col);
+		}
+	}
+}
+
+/** Gets the formatted initial array based on the file input
+ * @param input the file to search through
+ * @param rows the rows that should be in the returned array
+ * @param cols the columns that should be in the returned array
+ * @return the formatted 2D array of characters to be used as the initial condition.
+ */
+char** getInitial(FILE input, int rows, int cols){
+	char characters[(rows + 1)* cols]; //rows+1 * cols because we may need an extra column to store the newline characters
+	int newlines = 0; //newline characters, basically amount of rows
+	int charsSince = 0; //characters since last new line character
+	int maxCharsSince = 0; // maximum characters since last new line character, basically amount of columns
+	int index = 0; //index in characters;
+	int c; //int representing a character, basically a character or EOF.
+	//magic numbers:
+	char newlineChar = '\n'; //newline character
+	char endCharacter = '\0';//indicates that we've reached the end of the array. Could be done by counting newlines, but this is more readable IMO.
+	do { //EOF should be less than 0
+		c = getc(input); //get the next character.
+		if((c == newlineChar || c < 0 ) && charsSince > 0){ //if newline or end of file, and the line wasn't empty.
+			newlines++;
+			maxCharsSince = (maxCharsSince > charsSince) ? maxCharsSince : charsSince;
+			charsSince = 0;
+			characters[index++] = newlineChar;
+		}
+		else if (charsSince < cols){ //ignore any characters after columns that the array can't handle
+			if(c == FILLEDCHAR){
+				characters[index++] = FILLEDCHAR;
+				charsSince++;
+			}
+			else{
+				characters[index++] = UNFILLEDCHAR; //could technically be anything, but this is more fun
+				charsSince++;
+			}
+		}
+	} while (c > 0 && newlines < rows); //terminates after end of file or there are too many rows for the array to handle
+	characters[index] = endCharacter; //last character will ALWAYS be newline, but we set it to endCharacter now.
+	//characters is now propagated with all of the relevant characters from the file (characters that can actually be stored in the output array)
+	char output[rows][cols]; //the output array
+	for(int r = 0; r < rows; r++){
+		for(int c = 0; c < cols; c++){
+			output[r][c] = UNFILLEDCHAR; //make an empty board.
+		}
+	}
+	int startingRow = (rows/2) - (newlines/2); //used to store the initial condition in the approximate middle of the array.
+	int startingCol = (cols/2) - (maxCharsSince/2); //these are always 0 if the returned array should be full;
+	char nextChar; //the next character in the array.
+	index = 0; //reusing old index variable
+	for(int r = startingRow; r < rows; r++){
+		for(int c = startingCol; c < cols; c++){
+			nextChar = characters[index++]; //get the next character
+			if(nextChar == newlineChar) break; //everything after this in the row should be blank anyway, so go to the next row
+			if(nextChar == endCharacter) return output; //once we reach the end of the file, we are done. END returns the completed array.
+			if(nextChar == FILLEDCHAR) output[r][c] = FILLEDCHAR; //don't need to do anything for unfilled spaces
+		}
+	}
+
+
+	return output;
+}
---
+++ src/production.h
@@ -0,0 +1,13 @@
+/*
+ * production.h
+ *
+ *  Created on: Nov 3, 2018
+ *      Author: student
+ */
+
+#ifndef PRODUCTION_H_
+#define PRODUCTION_H_
+bool production(int argc, char* argv[]);
+void PlayOne (unsigned int nr, unsigned int nc, char Old[][nc], char New[][nc]);
+
+#endif /* PRODUCTION_H_ */
